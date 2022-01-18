423739e586c2ad20145a7741fbcfad37d7a3144f
galeno <gui.galenocover@gmail.com>
2022-01-18 13:12:29 -0300

    Dynamic Navigation

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,8 @@ export default function Home() {
       <Navigator texto="Estilo" destino="/estiloso" />
       <Navigator texto="Exemplo" destino="/example" />
       <Navigator texto="JSX" destino="/jsx" />
+      <Navigator texto="Navegação #01" destino="/navegation" />
+      <Navigator texto="Navegação #02" destino="/cliente/sp_2/123" />
     </div>
   );
 }
---
+++ src/pages/navegation/index.jsx
@@ -0,0 +1,9 @@
+import Layout from "../../components/Layout";
+
+export default function Navegation() {
+    return (
+        <Layout title="Exemplo de Navegação #01">
+            <h1>Navegação #01</h1>
+        </Layout>
+    )
+}
